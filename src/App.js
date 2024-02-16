2063ea65ab54ff0c18d835c31cde861c81482ebd
Thivagar R <thivagarradhakrishnan@gmail.com>
2024-02-16 12:54:25 +0530

    cm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, { useEffect } from 'react';
 import { Amplify } from 'aws-amplify';
-
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
 Amplify.configure(config);
+
 function App({ signOut, user }) {
+  useEffect(() => {
+    const redirectUser = () => {
+      window.location.href = 'http://trafyai.com/';
+    };
+
+    if (user) {
+      redirectUser();
+    }
+  }, [user]);
+
   return (
     <>
       <h1>Hello {user.username}</h1>
