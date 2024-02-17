4c4981f6bc59c256c278f39aa16586df91efab19
Thivagar R <thivagarradhakrishnan@gmail.com>
2024-02-17 14:19:08 +0530

    chan

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -7,16 +7,6 @@ import config from './amplifyconfiguration.json';
 Amplify.configure(config); // Configure Amplify
 
 function App(signOut,user) {
-  useEffect(() => {
-    async function redirectToExternalUrl() {
-      const user = await Amplify.Auth.currentAuthenticatedUser(); // Access Auth from Amplify directly
-      if (user) {
-        window.location.href = 'http://trafyai.com/';
-      }
-    }
-    redirectToExternalUrl();
-  }, []);
-
   return (
     <>
       <h1>Hello {user.username}</h1>
