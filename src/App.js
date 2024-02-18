4555f7c13354caf75ae0fbb3ea7484a1cb8abe04
Thivagar R <thivagarradhakrishnan@gmail.com>
2024-02-18 19:37:18 +0530

    chang m

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
 
-Amplify.configure(config); // Configure Amplify
+Amplify.configure(config);
 
-function App(signOut,user) {
+function App({ signOut, user }) {
+  // Function to redirect to https://trafyai.com/
+  const redirectToExternalSite = () => {
+    window.location.href = 'https://trafyai.com/';
+  };
+
   return (
     <>
       <h1>Hello {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
+      {/* Add a button to redirect to https://trafyai.com/ */}
+      <button onClick={redirectToExternalSite}>Go to Trafyai</button>
     </>
   );
 }
